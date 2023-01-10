5f39c8393679d7948eed8357acc3f5f0bd0b705a
Caloro <isabella.caloro@accenture.com>
2023-01-10 18:01:43 +0100

    add es

---
+++ Es.java
@@ -0,0 +1,15 @@
+ class Es{
+public static String duplicate(String s)
+{
+   String t = s + s;
+   return t;
+}
+
+public static void main(String[] args){
+    String s1 = "Hi";
+s1 = duplicate(s1);
+String t1 = "Bye";
+t1 = duplicate(duplicate(t1));
+System.out.println(s1 + t1);
+}
+}
