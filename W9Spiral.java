bf780ae3db9634a8d06149290ede6a7590927011
Caloro <isabella.caloro@accenture.com>
2023-01-19 14:10:01 +0100

    add Spiral

---
+++ W9Spiral.java
@@ -0,0 +1,14 @@
+public class W9Spiral {
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        double decay = Double.parseDouble(args[1]);
+        double angle = 360.0 / N;
+        double step = Math.sin(Math.toRadians(angle / 2.0));
+        W9Turtle turtle = new W9Turtle(0.5, 0, angle / 2.0);
+        for (int i = 0; i < 10 * N; i++) {
+            step /= decay;
+            turtle.goForward(step);
+            turtle.turnLeft(angle);
+        }
+    }
+}
