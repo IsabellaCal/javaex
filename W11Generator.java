2e4f501e8696cb2c8051979b7fab9e9a588b26b9
Caloro <isabella.caloro@accenture.com>
2023-01-20 10:53:24 +0100

    add generator

---
+++ W11Generator.java
@@ -0,0 +1,19 @@
+public class W1C2Generator {
+    // genera stringhe casuali mettendo come argomento il numero di stringe la lunghezza e l'alfabeto
+    public static String randomString(int L, String alpha) {
+        char[] a = new char[L];
+        for (int i = 0; i < L; i++) {
+            int t = StdRandom.uniformInt(alpha.length());
+            a[i] = alpha.charAt(t);
+        }
+        return new String(a);
+    }
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int L = Integer.parseInt(args[1]);
+        String alpha = args[2];
+        for (int i = 0; i < N; i++)
+            StdOut.println(randomString(L, alpha));
+    }
+}
